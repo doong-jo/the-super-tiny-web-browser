e0884a1e204e7cdd002e99b9c0770faa294cd6f7
doong-jo <sdong001@gmail.com>
2019-08-15 16:28:47 +0900

    refact url (regex, test)

--- app.test.js
+++ app.test.js
@@ -3,46 +3,39 @@ const
     URL = require('./url.js');
 
 let url = new URL("http://user_name:pass-word@boostcamp.connect-foundation.or.kr:2019/first/second/last?query=ab&param=12");
-console.log(url.getObject().absoluteString);
+console.log(url.absoluteString);
+
+console.log('add \"basecamp\" path');
 url.appendPathComponent("basecamp");
+console.log('add \"camp\" path');
 url.appendPathComponent("camp");
+
 //url.absoluteString = "http://user_name:pass-word@boostcamp.connect-foundation.or.kr:2019/first/second/last/basecamp/camp?query=ab&param=12"
-console.log(url.getObject().absoluteString);
+console.log(url.absoluteString);
+console.log('delete path');
 url.deleteLastPathComponent();
-console.log(url.getObject().absoluteString);
-//url.absoluteString = "http://user_name: pass-word@boostcamp.connect-foundation.or.kr:2019/first/second/last/basecamp?query=ab&param=12"
+console.log(url.absoluteString);
 
-var zumurl = new URL("http://admin@zum.com/#!/home?query=zum");
-
-var naverurl = new URL("http://m.naver.com");
-
-test('naverurl', () => {
-    expect(naverurl.getIsInValid()).toBe(false);
+var vaild_test_1 = new URL("http://m.naver.com");
+test('http://m.naver.com', () => {
+    expect(vaild_test_1.getIsInValid()).toBe(false);
 });
 
-var url1 = new URL("http://admin@zum.com/#!/home?query=zum");
-
-test('url1', () => {
-    expect(url1.getIsInValid()).toBe(true);
+var vaild_test_2 = new URL("http://.com");
+test('http://.com', () => {
+    expect(vaild_test_2.getIsInValid()).toBe(true);
 });
 
-var url2 = new URL("http://admin@zum.com/#!/home");
-
-test('url2', () => {
-    expect(url2.getIsInValid()).toBe(true);
+var vaild_test_3 = new URL("://m.naver.com");
+test('://m.naver.com', () => {
+    expect(vaild_test_3.getIsInValid()).toBe(true);
 });
 
-var url3 = new URL("http://admin@zum.com/?param=zum");
-
-test('url3', () => {
-    expect(url3.getIsInValid()).toBe(true);
+var vaild_test_4 = new URL("hi://m.naver.com");
+test('hi://m.naver.com', () => {
+    expect(vaild_test_4.getIsInValid()).toBe(true);
 });
 
-var url4 = new URL("http://zum.com/#!/home");
-
-test('url4', () => {
-    expect(url4.getIsInValid()).toBe(true);
-});
 
 var pivot_url = new URL("http://user_name:pass-word@naver.com:123/abc");
 
--- url.js
+++ url.js
@@ -17,6 +17,16 @@ Array.prototype.equals = function (array) {
     return true;
 }
 
+const REGEX = {
+    SCHEME: 1,
+    WWW: 2,
+    USER_INFO: 3,
+    HOST: 4,
+    PORT: 5,
+    PATH: 6,
+    QUERY: 7,
+};
+
 module.exports = class URL {
     constructor(url) {
         Object.assign(this, {
@@ -32,32 +42,78 @@ module.exports = class URL {
             user: "",
             password: "",
             absoluteString: "", 
-        });
 
+            www: "",
+        });
+        
         this.analysis(url);
     }
 
     // https://stackoverflow.com/a/5717133
-    invalidURL(str) {
-        // var format = /[!#*()+\[\];'"\\|<>]/;
+    validCheck(url) {
+        const regex = /(https?|file):\/\/(www\.)?([a-zA-Z0-9-:!#$%^&*()_+=-]*?[@])?([-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b)[:]?([0-9]*)?([-a-zA-Z0-9()@:%_\+.~#/=]*)?[?]?([-a-zA-Z0-9()@:%_\+.~#=?&]*)/g;
 
-        if(format.test(str)){
-            return true;
-        } else {
-            return false;
+        if( !url.match(regex) ) {
+            throw new Error("invalid url error (no match)");
         }
+
+        const regexArr = regex.exec(url);
+        /*
+            example
+            url = "http://www.user_name:pass-word@boostcamp.connect-foundation.or.kr:2019/first/second/last?query=ab&param=12"
+            [ 
+                'http://www.user_name:pass-word@boostcamp.connect-foundation.or.kr:2019/first/second/last?query=ab&param=12',
+                'http',
+                'www.',
+                'user_name:pass-word@',
+                'boostcamp.connect-foundation.or.kr',
+                '2019',
+                '/first/second/last',
+                'query=ab&param=12',
+                index: 0,
+                input: 'http://www.user_name:pass-word@boostcamp.connect-foundation.or.kr:2019/first/second/last?query=ab&param=12',
+                groups: undefined 
+            ]
+        */
+
+        
+        this.scheme = regexArr[REGEX.SCHEME];
+        this.www = regexArr[REGEX.WWW] ? regexArr[REGEX.WWW] : this.www;
+        this.isFileURL = this.scheme == "file" ? true : false;
+        this.host = regexArr[REGEX.HOST];
+        this.port = regexArr[REGEX.PORT] ? regexArr[REGEX.PORT] : this.port;
+        this.query = regexArr[REGEX.QUERY] ? regexArr[REGEX.QUERY] : this.query;
+        this.absoluteString = url;
+
+        if( regexArr[REGEX.USER_INFO] ) {
+            const user_info = regexArr[REGEX.USER_INFO].split(":");
+            console.log('user_info', user_info);
+            if( user_info.length > 1 ) { 
+                this.user = user_info[0];
+                this.password = user_info[1].slice(0, -1);
+            } else {
+                this.user = user_info[0].slice(0, -1);
+            }
+            
+             // remove @
+        }
+        
+        if( regexArr[REGEX.PATH] ) {
+            this.pathComponents = regexArr[REGEX.PATH].split("/");
+            this.pathComponents.shift(); // remove blank
+
+            this.lastPathComponent = this.pathComponents[this.pathComponents.length - 1];
+        }
+
+        this.invalid = false;
     }
 
     // TODO : write url regex
     analysis(url) {
         try {
-            if( this.invalidURL(url) ) {
-                this.invalid = true;
-                throw new Error("invalid url error");
-            }
-
-
+            this.validCheck(url);
         } catch (e) {
+            this.invalid = true;
             console.log(e.message);
         }
     }
@@ -73,10 +129,10 @@ module.exports = class URL {
     }
 
     replaceAbsoluteString() {
-        const user_info = this.user !== '' ? `${this.user}:${this.user}` : '';
+        const user_info = this.user !== '' ? `${this.user}:${this.password}` : '';
         const query = this.query !== '' ? `?${this.query}` : '';
         this.absoluteString = 
-            `${this.schema}://${user_info}@${this.host}:${this.port}${this.pathComponents.join('/')}${query}`;
+            `${this.scheme}://${this.www}${user_info}@${this.host}:${this.port}/${this.pathComponents.join('/')}${query}`;
     }
 
     getIsInValid() {
